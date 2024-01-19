49c697a7bd23f26a14e296bd70b697fab0f430a9
templeajuzie <templeajuzie@gmail.com>
2024-01-19 12:28:58 +0100

    fix product details and news details

--- screens/NewsDetails.js
+++ screens/NewsDetails.js
@@ -6,6 +6,7 @@ import { View, Text, Image, StatusBar } from "react-native";
 import { ScrollView, SafeAreaView } from "react-native";
 import globalstyels from "../styles/globalstyels";
 import { useRoute } from "@react-navigation/native";
+import HTML from "react-native-render-html";
 
 const NewsDetails = () => {
   const route = useRoute();
@@ -15,28 +16,28 @@ const NewsDetails = () => {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#2c3e50" />
+
       <SafeAreaView style={globalstyels.droidSafeArea}>
         <ScrollView className="py-2">
           <View className="container p-3 space-y-8">
             <View className="space-y-2 text-center">
               <Text className="text-3xl font-bold">{title}</Text>
             </View>
-            <View className=" grid grid-cols-1 gap-y-8 ">
+            <View className="grid grid-cols-1 gap-y-8">
               <View className="flex flex-col ">
                 <Image
                   alt=""
-                  className="object-cover w-full h-52 object-top rounded-t"
+                  className="object-cover object-top w-full rounded-t h-52"
                   source={{ uri: image }}
                   resizeMode="contain"
                   resizeMethod="resize"
                 />
                 <View className="flex flex-col flex-1 p-1">
-                  <Text className="text-xs tracki uppercase hover:underline text-blue-600">
+                  <Text className="text-xs text-blue-600 uppercase tracki hover:underline">
                     {category}
                   </Text>
-                  <Text className="flex-1 py-2 text-lg font-semibold w-full">
-                    {desc}
-                  </Text>
+
+                  <HTML source={{ html: desc }} />
                   <View className="flex flex-wrap justify-between pt-3 text-xs ">
                     <Text>June 1, 2020</Text>
                     <Text>2.1K views</Text>
--- screens/ProductDetails.js
+++ screens/ProductDetails.js
@@ -1,30 +1,49 @@
-import React from 'react'
-import { View, ScrollView, Image, Text, Button } from "react-native";
-import { useRoute} from "@react-navigation/native";
-
-
+import React from "react";
+import { View, ScrollView, Image, Text, Button, TouchableOpacity } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import HTML from "react-native-render-html";
 
 const ProductDetails = () => {
-     const route = useRoute();
-     const { title, price, description, thumbnail } = route.params;
+  const route = useRoute();
+  const { title, price, description, thumbnail } = route.params;
   return (
     <ScrollView style={styles.container}>
-      <Image
-        style={styles.image}
-        source={{ uri: thumbnail }}
-      />
-      <View style={styles.info}>
-              <Text style={styles.name}>{title}</Text>
-              <Text style={styles.price}>${price}</Text>
-        <Text style={styles.description}>
-        {description}
-        </Text>
+      <View className="mt-10 mb-40">
+        <View className="px-10">
+          <Image
+            style={styles.image}
+            source={{ uri: thumbnail }}
+            className="rounded-md"
+          />
+        </View>
+        <View className="flex flex-row items-start w-full gap-5 px-4 mt-6">
+          <TouchableOpacity
+            onPress={() => {}}
+            className="px-5 py-3 bg-blue-600 border-gray-100 rounded-md shadow-md border-1"
+          >
+            <Text className="text-lg font-semibold text-left text-white">
+              Add to now
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {}}
+            className="px-5 py-3 bg-blue-600 border-gray-100 rounded-md shadow-md border-1"
+          >
+            <Text className="text-lg font-semibold text-left text-white">
+              View cart
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.name}>{title}</Text>
+          <Text style={styles.price}>${price}</Text>
+          <HTML source={{ html: description }} />
+        </View>
       </View>
-      <Button title="Add to Cart" onPress={() => {}} />
     </ScrollView>
   );
-}
-
+};
 
 const styles = {
   container: {
@@ -54,4 +73,4 @@ const styles = {
   },
 };
 
-export default ProductDetails
+export default ProductDetails;
