fb78ba2947ae412a09d64aca7dbb1a4d2126e81e
Awajimijan Igoni <mijanigoni@gmail.com>
2024-01-20 01:56:01 +0100

    creating profile page

---
+++ components/svgs/Icons.js
@@ -0,0 +1,102 @@
+import React from 'react'
+
+
+import Svg, { G , Path } from "react-native-svg";
+
+export const AngleIcon =() =>{
+  return (
+    <Svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#737373"
+      
+    >
+      <Path
+        d="M10 8l4 4-4 4"
+        stroke="#737373"
+        strokeWidth={1.2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
+
+export const ChangePasswordIcon=() =>{
+  return (
+    <Svg
+      fill="#737373"
+      width="30px"
+      height="30px"
+      viewBox="0 0 512 512"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#737373"
+      
+    >
+      <Path d="M464.433 147.54a9.899 9.899 0 00-17.56 9.14 214.264 214.264 0 01-38.769 251.42c-83.856 83.848-220.315 83.875-304.207-.008a9.896 9.896 0 00-16.893 7.005v56.9a9.896 9.896 0 0019.793 0v-34.55A234.95 234.95 0 00464.433 147.54zM103.897 103.902c83.882-83.874 220.341-83.865 304.207-.009a9.89 9.89 0 0016.892-6.996v-56.9a9.896 9.896 0 00-19.793 0v34.55C313.023-1.356 176.055 3.751 89.904 89.901a233.956 233.956 0 00-42.337 274.553 9.899 9.899 0 0017.56-9.14 214.249 214.249 0 0138.77-251.412z" />
+      <Path d="M126.4 254.555v109.44a27.08 27.08 0 0027 27h205.2a27.077 27.077 0 0027-27v-109.44a27.078 27.078 0 00-27-27H153.4a27.08 27.08 0 00-27 27zM328 288.13a21.146 21.146 0 11-21.146 21.146A21.167 21.167 0 01328 288.13zm-72 0a21.146 21.146 0 11-21.146 21.146A21.167 21.167 0 01256 288.13zm-72 0a21.146 21.146 0 11-21.146 21.146A21.167 21.167 0 01184 288.13zM343.653 207.756v-36.002a87.653 87.653 0 00-175.306 0v36.002h19.793v-36.002a67.86 67.86 0 01135.72 0v36.002z" />
+    </Svg>
+  );
+}
+export const EditIcon=() =>{
+  return (
+    <Svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#737373"
+     
+    >
+      <G strokeWidth={1.176} strokeLinecap="round" strokeLinejoin="round">
+        <Path d="M21.28 6.4l-9.54 9.54c-.95.95-3.77 1.39-4.4.76-.63-.63-.2-3.45.75-4.4l9.55-9.55a2.58 2.58 0 113.64 3.65v0z" />
+        <Path d="M11 4H6a4 4 0 00-4 4v10a4 4 0 004 4h11c2.21 0 3-1.8 3-4v-5" />
+      </G>
+    </Svg>
+  );
+}
+export const CloseAccountIcon=() =>{
+  return (
+    <Svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#737373"
+    >
+      <Path
+        d="M15 19c0-2.21-2.686-4-6-4s-6 1.79-6 4m12-6h6M9 12a4 4 0 110-8 4 4 0 010 8z"
+        stroke="#737373"
+        strokeWidth={1.176}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
+export const LogOutIcon=() =>{
+  return (
+    <Svg
+      width="30px"
+      height="30px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#737373"
+     
+    >
+      <G strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.176}>
+        <Path d="M15 3H7a2 2 0 00-2 2v14a2 2 0 002 2h8M19 12l-4-4m4 4l-4 4m4-4H9" />
+      </G>
+    </Svg>
+  );
+}
+
+
+
+
--- screens/Cart.js
+++ screens/Cart.js
@@ -6,10 +6,12 @@ import {
   TouchableOpacity,
   Image,
   DatePickerIOS,
+  Pressable,
 } from "react-native";
 import React, { useState } from "react";
 import { UseProductProvider } from "../context/ProductProvider";
 import { StyleSheet } from "react-native";
+import Svg, { Path } from "react-native-svg";
 
 
 export default function Cart() {
@@ -38,34 +40,54 @@ export default function Cart() {
           {allProducts.map((product) => (
             <View
               key={product._id} // Add a unique key prop for each item in the array
-             
-
               className="bg-white flex flex-row px-[8px] py-2 justify-between items-center mb-2 mx-2 rounded-sm shadow-md"
             >
-              <Image
-                style={{ height: 100, width: 100 }}
-                source={{
-                  uri: product.thumbnail,
-                }}
-              />
+              <View className="flex flex-row items-center ">
+                <Image
+                  style={{ height: 100, width: 100 }}
+                  source={{
+                    uri: product.thumbnail,
+                  }}
+                />
 
-              <View className="flex flex-row border border-gray-200 items-center justify-between h-10  rounded-sm px-2 ">
-                <TouchableOpacity className="text-[#333] h-full w-6 flex items-center justify-center">
-                  <Text className="text-[#333]">-</Text>
-                </TouchableOpacity>
-                <View className="w-6 bg-gray-100 h-full flex items-center justify-center">
-                  <Text>2</Text>
+                <View className="flex flex-col gap-4 w-[40%]  ">
+                  <Text className="font-bold text-sm whitespace-nowrap line-clamp-1">
+                    {product.title}
+                  </Text>
+
+                  <Text className="font-bold text-sm whitespace-nowrap">
+                    ${product.price.toFixed(2)}
+                  </Text>
                 </View>
-
-                <TouchableOpacity className="text-[#333]   h-full w-6 flex items-center justify-center">
-                  <Text className="text-[#333]">+</Text>
-                </TouchableOpacity>
               </View>
+              <View className="flex flex-col gap-4 ">
+                <Pressable>
+                  <Svg
+                    viewBox="-0.5 0 19 19"
+                    xmlns="http://www.w3.org/2000/svg"
+                   
+                  >
+                    <Path
+                      d="M4.917 14.889c0 .468.687 1.111 1.146 1.111h6.875c.458 0 1.145-.643 1.145-1.111V6H4.917v8.889zM15 3.465h-2.444L11.333 2H7.667L6.444 3.465H4V4.93h11V3.465z"
+                      fill="#000"
+                      stroke="none"
+                      strokeWidth={1}
+                      fillRule="evenodd"
+                    />
+                  </Svg>
+                </Pressable>
+                <View className="flex flex-row border border-gray-200 items-center justify-between h-10  rounded-sm px-2 ">
+                  <TouchableOpacity className="text-[#333] h-full w-6 flex items-center justify-center">
+                    <Text className="text-[#333]">-</Text>
+                  </TouchableOpacity>
+                  <View className="w-6 bg-gray-100 h-full flex items-center justify-center">
+                    <Text>2</Text>
+                  </View>
 
-              <View>
-                <Text className="font-bold text-sm">
-                  ${product.price.toFixed(2)}
-                </Text>
+                  <TouchableOpacity className="text-[#333]   h-full w-6 flex items-center justify-center">
+                    <Text className="text-[#333]">+</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           ))}
@@ -84,9 +106,7 @@ export default function Cart() {
             </View>
           </View>
           <TouchableOpacity className="bg-black flex flex-row justify-center items-center h-10">
-            <Text className="text-white">
-                Confirm Order
-             </Text>
+            <Text className="text-white">Confirm Order</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
