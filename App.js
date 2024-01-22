fe0f6c9034ec5251ea30d9f034c695c98529e7db
princeajuzie7 <princeajuzie7@gmail.com>
2024-01-22 22:50:27 +0100

    update

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import ProductProvider from "./context/ProductProvider";
 import DrawerNavigator from "./navigation/DrawerNavigator";
-
+import { NativeWindStyleSheet } from "nativewind";
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
