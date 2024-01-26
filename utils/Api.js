a9540d29a5b602ab6ae1412a018fa549ff645d9c
princeajuzie7 <princeajuzie03@gmail.com>
2024-01-26 17:59:13 +0100

    feat:new page added

--- utils/Api.js
+++ utils/Api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-import {APP_ENV} from '@env';
+import {EXPO_PUBLIC_SERVER_URL} from '@env';
 export default axios.create({
-  baseURL: APP_ENV,
+  baseURL: EXPO_PUBLIC_SERVER_URL,
   withCredentials: true,
   headers: {
     Accept: "application/json",
