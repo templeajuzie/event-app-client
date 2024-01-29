c53a2720c54cbf04b5eac67269d0f61b81eda546
princeajuzie7 <princeajuzie03@gmail.com>
2024-01-29 17:18:32 +0100

    feat:changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,32 +1,37 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity, Pressable,Image } from 'react-native'
-import React, { useState } from 'react'
-import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-import { UseUserContext } from '../context/UserContext';
-import { HamburgerIcon } from './svgs/Icons';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Pressable,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { UseUserContext } from "../context/UserContext";
+import { HamburgerIcon } from "./svgs/Icons";
 
-
-
-export default  function Navbar() {
-    const navigation=useNavigation()
-    const {UserData} = UseUserContext()
+export default function Navbar() {
+  const navigation = useNavigation();
+  const { UserData } = UseUserContext();
   const [loading, setLoading] = useState(false);
-  const {setIsSignUpVisible}= UseUserContext()
+  const { setIsSignUpVisible } = UseUserContext();
 
-    // set a timeout to change the state of the user then after some time set it to null
-// const token = JSON.parse(await AsyncStorage.getItem("authToken"))
+  // set a timeout to change the state of the user then after some time set it to null
+  // const token = JSON.parse(await AsyncStorage.getItem("authToken"))
 
-
-
-    return (
-      <View className="flex flex-row gap-3 items-center h-[65px] px-2 bg-[#2c3e50]">
-        <Pressable
-          className="flex-1 flex flex-row items-center boder"
-          onPress={() => navigation.openDrawer()}
-        >
-          <HamburgerIcon/>
-        </Pressable>
-        {/* {user ? (
+  return (
+    <SafeAreaView className="flex flex-row gap-3 items-center h-[80px] px-2 bg-[#2c3e50]">
+      <Pressable
+        className="flex-1 flex flex-row items-center boder"
+        onPress={() => navigation.openDrawer()}
+      >
+        <HamburgerIcon />
+      </Pressable>
+      {/* {user ? (
           <View className="flex-1 flex flex-row items-center justify-center">
             <Text
               onPress={() => setUser(null)}
@@ -36,8 +41,16 @@ export default  function Navbar() {
             </Text>
           </View>
         ) : ( */}
-        <View className="flex-1 flex flex-row items-center justify-end">
-          <View className="flex flex-row gap-1 items-center justify-center p-1 rounded">
+      <View className="flex-1 flex flex-row items-center justify-end">
+        <View className="flex flex-row gap-1 items-center justify-center p-1 rounded">
+          {UserData.authtoken !== "" ? (
+            <TouchableOpacity>
+              <Image
+                source={UserData && UserData.userdp}
+                className="w-10 h-10  rounded-full border-2 border-[#f5f5f5]"
+              />
+            </TouchableOpacity>
+          ) : (
             <TouchableOpacity
               onPress={() => setIsSignUpVisible(true)}
               activeOpacity={0.5}
@@ -47,8 +60,9 @@ export default  function Navbar() {
                 Login / signup
               </Text>
             </TouchableOpacity>
-          </View>
+          )}
         </View>
       </View>
-    );
+    </SafeAreaView>
+  );
 }
